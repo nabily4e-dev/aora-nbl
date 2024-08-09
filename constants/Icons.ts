e58eb26202d717d1ac1fee25aa6af930d6fbeed9
NabiL Ashbat <nbl.dev21@gmail.com>
2024-08-09 18:11:06 +0300

    Initial commit

---
+++ constants/Icons.ts
@@ -0,0 +1,29 @@
+import bookmark from '../assets/Icons/bookmark.png';
+import home from '../assets/Icons/home.png';
+import plus from '../assets/Icons/plus.png';
+import profile from '../assets/Icons/profile.png';
+import leftArrow from '../assets/Icons/left-arrow.png';
+import menu from '../assets/Icons/menu.png';
+import search from '../assets/Icons/search.png';
+import upload from '../assets/Icons/upload.png';
+import rightArrow from '../assets/Icons/right-arrow.png';
+import logout from '../assets/Icons/logout.png';
+import eyeHide from '../assets/Icons/eye-hide.png';
+import eye from '../assets/Icons/eye.png';
+import play from '../assets/Icons/play.png';
+
+export default {
+  play,
+  bookmark,
+  home,
+  plus,
+  profile,
+  leftArrow,
+  menu,
+  search,
+  upload,
+  rightArrow,
+  logout,
+  eyeHide,
+  eye,
+};
---
+++ constants/Images.ts
@@ -0,0 +1,9 @@
+import profile from '../assets/Images/profile.png';
+import thumbnail from '../assets/Images/thumbnail.png';
+import cards from '../assets/Images/cards.png';
+import path from '../assets/Images/path.png';
+import logo from '../assets/Images/logo.png';
+import logoSmall from '../assets/Images/logo-small.png';
+import empty from '../assets/Images/empty.png';
+
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
